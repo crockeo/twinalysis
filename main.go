98ca36436a8687c19925e51c1561844bd9bfc001
Cerek Hillen <chillen@lyft.com>
2020-07-26 21:03:40 -0400

    refactors!

--- main.go
+++ main.go
@@ -2,13 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io/ioutil"
 	"os"
-
-	"github.com/dghubble/go-twitter/twitter"
-
-	"golang.org/x/oauth2"
-	"golang.org/x/oauth2/clientcredentials"
 
 	"github.com/crockeo/twinalysis/module"
 	"github.com/crockeo/twinalysis/module/averages"
@@ -16,43 +10,10 @@ import (
 )
 
 const (
-	API_KEY_FILE        string = "secrets/api"
-	API_SECRET_KEY_FILE string = "secrets/api_secret"
-	BEARER_TOKEN_FILE   string = "secrets/bearer"
-
 	DEFAULT_PERMS os.FileMode = 0755
 )
 
 var MODULES = []module.Module{averages.Averages{}}
-
-func readKey(path string) (string, error) {
-	rawKey, err := ioutil.ReadFile(path)
-	if err != nil {
-		return "", err
-	}
-	return string(rawKey), nil
-}
-
-func twitterClient() (*twitter.Client, error) {
-	apiKey, err := readKey(API_KEY_FILE)
-	if err != nil {
-		return nil, err
-	}
-	apiKeySecret, err := readKey(API_SECRET_KEY_FILE)
-	if err != nil {
-		return nil, err
-	}
-
-	config := &clientcredentials.Config{
-		ClientID:     apiKey,
-		ClientSecret: apiKeySecret,
-		TokenURL:     "https://api.twitter.com/oauth2/token",
-	}
-	httpClient := config.Client(oauth2.NoContext)
-	client := twitter.NewClient(httpClient)
-
-	return client, nil
-}
 
 func getModule(name string) (*module.Module, error) {
 	for _, module := range MODULES {
@@ -64,11 +25,6 @@ func getModule(name string) (*module.Module, error) {
 }
 
 func main() {
-	client, err := twitterClient()
-	if err != nil {
-		panic(err)
-	}
-
 	if len(os.Args) < 3 {
 		fmt.Println("Insufficient arguments")
 		return
@@ -80,26 +36,29 @@ func main() {
 		return
 	}
 
-	tweetEntryChan := make(chan module.TweetEntry, 10)
-	errChan := make(chan error)
+	client, err := tweets.NewClient()
+	if err != nil {
+		panic(err)
+	}
 
-	go (*mod).AnalyzeTweets(tweetEntryChan, errChan)
-	err = tweets.CollectTweets(client, tweetEntryChan, os.Args[2:])
+	errChan := make(chan error)
+	defer close(errChan)
+
+	go (*mod).AnalyzeTweets(client.Chan(), errChan)
+	err = client.GetTweetsForUsers(os.Args[2:])
 	if err != nil {
 		panic(err)
 	}
 
 	for {
-		if len(tweetEntryChan) == 0 {
+		if len(client.Chan()) == 0 {
 			break
 		}
 	}
-	close(tweetEntryChan)
+	client.Close()
 
 	err = <-errChan
 	if err != nil {
 		panic(err)
 	}
-
-	close(errChan)
 }
--- tweets/tweets.go
+++ tweets/tweets.go
@@ -10,12 +10,141 @@ import (
 
 	"github.com/dghubble/go-twitter/twitter"
 
+	"golang.org/x/oauth2"
+	"golang.org/x/oauth2/clientcredentials"
+
 	"github.com/crockeo/twinalysis/module"
 )
 
 const (
+	API_KEY_FILE        string = "secrets/api"
+	API_SECRET_KEY_FILE string = "secrets/api_secret"
+	BEARER_TOKEN_FILE   string = "secrets/bearer"
+
 	DEFAULT_PERMS os.FileMode = 0755
 )
+
+func readKey(path string) (string, error) {
+	rawKey, err := ioutil.ReadFile(path)
+	if err != nil {
+		return "", err
+	}
+	return string(rawKey), nil
+}
+
+type Client struct {
+	twitterClient  *twitter.Client
+	tweetEntryChan chan module.TweetEntry
+}
+
+func NewClient() (*Client, error) {
+	apiKey, err := readKey(API_KEY_FILE)
+	if err != nil {
+		return nil, err
+	}
+	apiKeySecret, err := readKey(API_SECRET_KEY_FILE)
+	if err != nil {
+		return nil, err
+	}
+
+	config := &clientcredentials.Config{
+		ClientID:     apiKey,
+		ClientSecret: apiKeySecret,
+		TokenURL:     "https://api.twitter.com/oauth2/token",
+	}
+	httpClient := config.Client(oauth2.NoContext)
+	twitterClient := twitter.NewClient(httpClient)
+
+	return &Client{
+		twitterClient:  twitterClient,
+		tweetEntryChan: make(chan module.TweetEntry, 10),
+	}, nil
+}
+
+func (c *Client) Chan() <- chan module.TweetEntry {
+	return c.tweetEntryChan
+}
+
+func (c *Client) Close() {
+	close(c.tweetEntryChan)
+}
+
+func (c *Client) GetTweetsForUser(username string) error {
+	cwd, err := os.Getwd()
+	if err != nil {
+		return err
+	}
+	cacheDir := path.Join(cwd, "data", username)
+	files, err := ioutil.ReadDir(cacheDir)
+	if err != nil {
+		err = os.MkdirAll(cacheDir, DEFAULT_PERMS)
+		if err != nil {
+			return err
+		}
+		files = []os.FileInfo{}
+	}
+
+	// reads contents of existing cache
+	var maxCachedID int64
+	for _, file := range files {
+		contents, err := ioutil.ReadFile(path.Join(cacheDir, file.Name()))
+		if err != nil {
+			return err
+		}
+
+		var tweet twitter.Tweet
+		err = json.Unmarshal(contents, &tweet)
+		if err != nil {
+			return err
+		}
+
+		c.tweetEntryChan <- module.TweetEntry{
+			Tweet:    tweet,
+			Username: username,
+		}
+
+		if maxCachedID < tweet.ID {
+			maxCachedID = tweet.ID
+		}
+	}
+
+	// fetches new tweets after the provided ID
+	err = fetchTweets(c.twitterClient, c.tweetEntryChan, username, cacheDir, maxCachedID)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (c *Client) GetTweetsForUsers(usernames []string) error {
+	var wg sync.WaitGroup
+	wg.Add(len(usernames))
+
+	errChan := make(chan error)
+	defer close(errChan)
+
+	for _, username := range usernames {
+		func(username string) {
+			err := c.GetTweetsForUser(username)
+			if err != nil {
+				select {
+				case errChan <- err:
+				default:
+				}
+			}
+			wg.Done()
+		}(username)
+	}
+
+	wg.Wait()
+	select {
+	case err := <-errChan:
+		return err
+	default:
+	}
+
+	return nil
+}
 
 func fetchTweets(
 	client *twitter.Client,
@@ -34,7 +163,7 @@ func fetchTweets(
 
 	// collect tweets from the twitter API
 	go func() {
-		defer func () { quitChan <- 0 }()
+		defer func() { quitChan <- 0 }()
 
 		excludeReplies := false
 		includeRetweets := false
@@ -99,75 +228,4 @@ func fetchTweets(
 	case <-quitChan:
 		return nil
 	}
-}
-
-func CollectTweets(client *twitter.Client, tweetEntryChan chan<- module.TweetEntry, usernames []string) error {
-	cwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-
-	var wg sync.WaitGroup
-	wg.Add(len(usernames))
-
-	errChan := make(chan error)
-	defer close(errChan)
-
-	for _, username := range usernames {
-		go func(username string) {
-			defer wg.Done()
-
-			cacheDir := path.Join(cwd, "data", username)
-			files, err := ioutil.ReadDir(cacheDir)
-			if err != nil {
-				err = os.MkdirAll(cacheDir, DEFAULT_PERMS)
-				if err != nil {
-					errChan <- err
-					return
-				}
-				files = []os.FileInfo{}
-			}
-
-			// reads contents of existing cache
-			var maxCachedID int64
-			for _, file := range files {
-				contents, err := ioutil.ReadFile(path.Join(cacheDir, file.Name()))
-				if err != nil {
-					errChan <- err
-					return
-				}
-
-				var tweet twitter.Tweet
-				err = json.Unmarshal(contents, &tweet)
-				if err != nil {
-					errChan <- err
-					return
-				}
-
-				tweetEntryChan <- module.TweetEntry{
-					Tweet:    tweet,
-					Username: username,
-				}
-
-				if maxCachedID < tweet.ID {
-					maxCachedID = tweet.ID
-				}
-			}
-
-			// fetches new tweets after the provided ID
-			err = fetchTweets(client, tweetEntryChan, username, cacheDir, maxCachedID)
-			if err != nil {
-				errChan <- err
-			}
-		}(username)
-	}
-
-	wg.Wait()
-	select {
-	case err = <- errChan:
-		return err
-	default:
-	}
-
-	return nil
 }
